a980595548b7b913717afd1f3ea6732602780238
alsongard <alsongard@gmail.com>
2025-01-21 13:09:33 +0200

    Header component done

--- rapid_api_games/src/components/header.js
+++ rapid_api_games/src/components/header.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 function Header({darkMode, setDarkMode})
 {
     function handleChange()
@@ -8,14 +8,20 @@ function Header({darkMode, setDarkMode})
         })
     };
     return(
-        <header className="dark:bg-slate-500 justify-between flex flex-row pl-[30px] px-[50px]">
-            <nav  className="flex flex-row w-[30%] justify-between list-none">
-                <li>Home</li>
-                <li>PC GAMES</li>
-                <li>Web Browser</li>
-            </nav>
-            <button onClick={handleChange}>Dark</button>
-        </header>
+        <div>
+
+            <header className="dark:bg-slate-500 py-[10px] justify-between flex flex-row pl-[30px] px-[50px] bg-gradient-to-tr from-[rgba(125,212,201,0.69)] to-[#323232]">
+                <nav  className="flex flex-row w-[30%] justify-between list-none">
+                    <li>Home</li>
+                    <li>PC GAMES</li>
+                    <li>Web Browser</li>
+                </nav>
+                <button onClick={handleChange}>Dark</button>
+
+            </header>
+            <Outlet/>
+
+        </div>
     )
 }
 export default Header;
