35f3e137c31487b47c161eed3938c4361b14d230
alsongard <alsongard@gmail.com>
2025-01-21 13:08:11 +0200

    Page Routes working solution <Outlet/> header component

--- rapid_api_games/src/components/Page.js
+++ rapid_api_games/src/components/Page.js
@@ -3,24 +3,24 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./homePage";
 import Footer from "./footer";
 import Header from "./header";
-
+import Testing from "./testingComp";
 
 function Page()
 {
     const [darkMode, setDarkMode] = React.useState(false);
-    // function handleChange()
-    // {
-    //     setDarkMode((prevValue)=>{
-    //         return !prevValue;
-    //     })
-    // };
 
     const answer = darkMode ? "dark" : "";
     return(
         <main className={`${answer}`}>
-            <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
-            <Footer/>
-            <Home/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<div> <Header darkMode={darkMode} setDarkMode={setDarkMode}/>  <Footer/></div>}>
+                        <Route index element={<Home/>} />
+                        <Route path="test"  element={<Testing/>} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+
         </main>
     )
 }
