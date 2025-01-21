a4bf4f89e2b6c876593faffaac0451954cc1e360
alsongard <alsongard@gmail.com>
2025-01-21 13:10:35 +0200

    testing component

---
+++ rapid_api_games/src/components/testingComp.js
@@ -0,0 +1,9 @@
+function Testing()
+{
+    return (
+        <div className="dark:bg-slate-500 text-black">
+            <h1 className="text-black" >Hello Testing</h1>
+        </div>
+    )
+}
+export default Testing;
