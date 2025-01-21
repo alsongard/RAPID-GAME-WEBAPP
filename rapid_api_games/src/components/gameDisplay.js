64bf92eb9dfb25df512faff1f163b00a2d332343
alsongard <alsongard@gmail.com>
2025-01-21 13:09:14 +0200

    stying images with border radius

--- rapid_api_games/src/components/gameDisplay.js
+++ rapid_api_games/src/components/gameDisplay.js
@@ -1,9 +1,9 @@
 function GameComponent(props)
 {
     return (
-        <div className="dark:border-white border-2 my-[20px]">
+        <div className="dark:border-white my-[20px]">
             <h2 className="dark:text-white"> Title: {props.item.title}</h2>
-            <a href={props.item.game_url}><img src={props.item.thumbnail} alt="game"/></a>
+            <a lcaa href={props.item.game_url}><img className="rounded" src={props.item.thumbnail} alt="game"/></a>
         </div>
     )
 }
