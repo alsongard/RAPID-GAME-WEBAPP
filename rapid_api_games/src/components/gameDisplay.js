7e7de3be8d9be7fafae8cee078d604854a601270
alsongard <alsongard@gmail.com>
2025-01-24 00:03:02 +0200

    images set link to gameInfo page

--- rapid_api_games/src/components/gameDisplay.js
+++ rapid_api_games/src/components/gameDisplay.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 function GameComponent(props)
 {
     return (
         <div className="dark:border-white my-[20px]">
             <h2 className="dark:text-white"> Title: {props.item.title}</h2>
-            <a lcaa href={props.item.game_url}><img className="rounded" src={props.item.thumbnail} alt="game"/></a>
+            <Link to={`/gameInfo/${props.item.id}` }><img className="rounded" src={props.item.thumbnail} alt="game"/></Link> 
         </div>
     )
 }
 
 export default GameComponent;
+
+// <a  href={props.item.game_url}><img className="rounded" src={props.item.thumbnail} alt="game"/></a>
