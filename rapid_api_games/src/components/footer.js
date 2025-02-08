5cfa6a770addbc3793d0ced9ba0cdddbd12ce184
alsongard <alsongard@gmail.com>
2025-02-09 01:20:22 +0200

    footer setup

--- rapid_api_games/src/components/footer.js
+++ rapid_api_games/src/components/footer.js
@@ -1,9 +1,14 @@
 function Footer()
 {
     return (
-        <footer className="dark:bg-slate-500">
-            <h1 className="dark:text-white">RAPID GAMES API</h1>
-        </footer>
+        <footer className="bg-gray-900 text-white py-6 pt-10">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">&copy; {new Date().getFullYear()} Rapid Games. All rights reserved.</p>
+
+          <p className="mt-[20px]">Made by AlsonGard</p>
+        </div>
+      </footer>
     )
 }
 export default Footer;
+  
